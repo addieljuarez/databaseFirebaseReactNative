31487a75a843f7ff6c40d346711129ec51ebf33b
Addiel Juarez <addieljuarez@gmail.com>
2017-03-27 18:28:16 -0600

    prompt android

--- index.android.js
+++ index.android.js
@@ -1,3 +1,58 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
+//
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+//
+// export default class demoFirebase extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.android.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Double tap R on your keyboard to reload,{'\n'}
+//           Shake or press menu button for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+//
+// AppRegistry.registerComponent('demoFirebase', () => demoFirebase);
+
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,47 +62,171 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  Alert,
 } from 'react-native';
+import prompt from 'react-native-prompt-android'
+
+
+const firebase = require('firebase');
+
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js');
+// const prompt = require('react-native-prompt-android');
+
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATOxcXEFig85a5o6mhNK46B0Op4vfLpx8",
+  authDomain: "reactfiredemo1.firebaseapp.com",
+  databaseURL: "https://reactfiredemo1.firebaseio.com/",
+  storageBucket: "",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+
 
 export default class demoFirebase extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
+  _addItem() {
+    // Alert.prompt(
+    //   'Add New Item',
+    //   null,
+    //   [
+    //     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+    //     {
+    //       text: 'Add',
+    //       onPress: (text) => {
+    //         this.itemsRef.push({ title: text })
+    //       }
+    //     },
+    //   ],
+    //   'plain-text'
+    // );
+
+    prompt(
+      'Add New Item',
+      null,
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      {
+        // type: 'secure-text',
+        type: 'plain-text',
+        cancelable: false,
+        defaultValue: '',
+        placeholder: ''
+      }
+    );
+
+
+    // Alert.alert(
+    //   'Alert Title',
+    //   'My Alert Msg',
+    //   [
+    //     {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+    //     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+    //     {text: 'OK', onPress: () => console.log('OK Pressed')},
+    //   ],
+    //   { cancelable: false }
+    // )
+      // return (
+      //   <View style={{backgroundColor:'red', flex:1}}></View>
+      // );
+
+
+
+  }
+
+  _renderItem(item) {
+
+    const onPress = () => {
+      AlertIOS.alert(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
+    );
+  }
+
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('demoFirebase', () => demoFirebase);
